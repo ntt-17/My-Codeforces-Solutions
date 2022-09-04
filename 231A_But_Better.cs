21337e9dcfb5a93ef7469234b88b5247686fd606
Tai Nguyen <69973279+ntt-17@users.noreply.github.com>
2022-09-04 11:11:37 +0700

    Add files via upload

---
+++ 231A_But_Better.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codeforces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, count = 0;
+
+            n = Convert.ToInt32(Console.ReadLine());
+            
+            while (n > 0)
+            {
+                string[] numList = Console.ReadLine().Split(' ');
+                int Petya = Convert.ToInt32(numList[0]);
+                int Vasya = Convert.ToInt32(numList[1]);
+                int Tonya = Convert.ToInt32(numList[2]);
+
+                if (Petya + Vasya + Tonya >= 2)
+                    count++;
+                n--;
+            }
+            Console.WriteLine(count);
+            Console.ReadLine();
+        }
+    }
+}
