d977d4588104464043b102cbd7e82ad8a139a710
Tai Nguyen <69973279+ntt-17@users.noreply.github.com>
2022-09-18 16:47:09 +0700

    Add files via upload

---
+++ 158A.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 158A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, k;
+            int count = 0;
+            string[] numList = Console.ReadLine().Split(' ');
+            n = Convert.ToInt32(numList[0]);
+            k = Convert.ToInt32(numList[1]);
+
+            int[] a = new int[n];
+            string[] numList1 = Console.ReadLine().Split(' ');
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = Convert.ToInt32(numList1[i]);
+            }
+
+            for(int i = 0; i < n; i++)
+            {
+                if (a[i] >= a[k - 1] && a[i] != 0)
+                    count++;
+            }
+            Console.WriteLine(count);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 50A.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 50A
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int n, m;
+
+            string[] numList = Console.ReadLine().Split(' ');
+            n = Convert.ToInt32(numList[0]);
+            m = Convert.ToInt32(numList[1]);
+
+            int result = (n * m) / 2;
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
