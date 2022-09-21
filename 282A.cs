f6d08dee867baad5eca3071da6f0b3d5012e25d8
Tai Nguyen <69973279+ntt-17@users.noreply.github.com>
2022-09-21 03:21:21 -0700

    Add files via upload

---
+++ 282A.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codeforces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int X = 0;
+            int n;
+
+            n = Convert.ToInt32(Console.ReadLine());
+            string[] a = new string[n];
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = Console.ReadLine();
+                if (a[i] == "X++")
+                {
+                    X++;
+                }
+                else if (a[i] == "++X")
+                {
+                    X = ++X;
+                }
+
+                if (a[i] == "X--")
+                {
+                    X--;
+                }
+                else if (a[i] == "--X")
+                {
+                    X = --X;
+                }
+            }
+            Console.WriteLine(X);
+            Console.ReadLine();
+            
+        }
+    }
+}
