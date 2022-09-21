13ea78c90f5a12a0881be2c5f78a6b6137b4adf6
Tai Nguyen <69973279+ntt-17@users.noreply.github.com>
2022-09-21 09:47:11 -0700

    Add files via upload

---
+++ 263A.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codeforces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 0;
+            int c;
+            for (int row = 0; row < 5; row++)
+            {
+                for (int col = 0; col < 5; col++)
+                {
+                    c = Convert.ToInt32(Console.ReadLine());
+                    if (c == 1)
+                    {
+                        result = Math.Abs(row - 2) + Math.Abs(col - 2);
+                    }
+                }
+            }
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
